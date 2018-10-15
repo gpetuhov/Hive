36670077b1e787b4e8096e5abedc27618289ce4a
Gennady Petukhov <gpetukhov@phoenixit.ru>
2018-10-15 13:08:21 +0300

    Add navigation graph and bottom navigation view

--- app/src/main/java/com/gpetuhov/android/hive/MainActivity.kt
+++ app/src/main/java/com/gpetuhov/android/hive/MainActivity.kt
@@ -2,11 +2,25 @@ package com.gpetuhov.android.hive
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Find NavController
+        navController = findNavController(R.id.nav_host)
+
+        // Tie NavHostFragment to bottom navigation bar
+        navigationView.setupWithNavController(navController)
     }
+
+    override fun onSupportNavigateUp() = navController.navigateUp()
 }
