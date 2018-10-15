efe27c5b3c3d6b9e6148c7dbcbf942dc59afa665
Gennady Petukhov <gpetukhov@phoenixit.ru>
2018-10-15 18:08:44 +0300

    Place marker on map and zoom to it

--- app/src/main/java/com/gpetuhov/android/hive/ui/fragment/MapFragment.kt
+++ app/src/main/java/com/gpetuhov/android/hive/ui/fragment/MapFragment.kt
@@ -5,11 +5,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.MapView
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import com.gpetuhov.android.hive.R
 
-class MapFragment : Fragment() {
+class MapFragment : Fragment(), OnMapReadyCallback {
 
     private lateinit var googleMap: GoogleMap
     private lateinit var mapView: MapView
@@ -21,18 +26,7 @@ class MapFragment : Fragment() {
         mapView.onCreate(savedInstanceState)
 
         // Asynchronously get reference to the map
-        mapView.getMapAsync { map ->
-            // When the map is ready, get reference to it
-            googleMap = map
-
-            // Enable zoom buttons
-            googleMap.uiSettings.isZoomControlsEnabled = true
-
-            // When the map is loaded, do something
-            googleMap.setOnMapLoadedCallback {
-                // TODO: do something
-            }
-        }
+        mapView.getMapAsync(this)
 
         return view
     }
@@ -55,5 +49,30 @@ class MapFragment : Fragment() {
     override fun onLowMemory() {
         super.onLowMemory()
         mapView.onLowMemory()
+    }
+
+    override fun onMapReady(map: GoogleMap) {
+        // When the map is ready, get reference to it
+        googleMap = map
+
+        // Enable zoom buttons
+        googleMap.uiSettings.isZoomControlsEnabled = true
+
+        // For showing a move to my location button
+        // TODO: grant permissions and check if granted
+//        googleMap.setMyLocationEnabled(true)
+
+        // For dropping a marker at a point on the Map
+        val sydney = LatLng(-34.0, 151.0)
+        googleMap.addMarker(MarkerOptions().position(sydney).title("Marker Title").snippet("Marker Description"))
+
+        // For zooming automatically to the location of the marker
+        val cameraPosition = CameraPosition.Builder().target(sydney).zoom(12.0F).build()
+        googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition))
+
+        // When the map is loaded, do something
+        googleMap.setOnMapLoadedCallback {
+            // TODO: do something
+        }
     }
 }
