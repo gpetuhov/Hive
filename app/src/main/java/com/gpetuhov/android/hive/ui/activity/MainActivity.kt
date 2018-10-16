c2923b8097215849a46a3b49044d7787a6f59ce3
Gennady Petukhov <gpetukhov@phoenixit.ru>
2018-10-16 16:43:34 +0300

    Finish main acitivity before showing permissions screen

--- app/src/main/java/com/gpetuhov/android/hive/ui/activity/MainActivity.kt
+++ app/src/main/java/com/gpetuhov/android/hive/ui/activity/MainActivity.kt
@@ -36,6 +36,7 @@ class MainActivity : AppCompatActivity() {
         if (checkPlayServices()) {
             if (!checkPermissions(this)) {
                 startActivity<PermissionsActivity>()
+                finish()
             }
         }
     }
