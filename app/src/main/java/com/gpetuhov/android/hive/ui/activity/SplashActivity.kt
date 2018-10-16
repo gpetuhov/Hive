0bb58202d937b3c588b18262788f973df96ddac0
Gennady Petukhov <gpetukhov@phoenixit.ru>
2018-10-16 18:39:39 +0300

    Init Google Maps during splash screen

--- app/src/main/java/com/gpetuhov/android/hive/ui/activity/SplashActivity.kt
+++ app/src/main/java/com/gpetuhov/android/hive/ui/activity/SplashActivity.kt
@@ -2,6 +2,7 @@ package com.gpetuhov.android.hive.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.maps.MapView
 import com.pawegio.kandroid.startActivity
 
 // Implementing splash screen by setting
@@ -20,13 +21,25 @@ import com.pawegio.kandroid.startActivity
 // has already been started and it is time to hide splash screen.
 class SplashActivity : AppCompatActivity() {
 
+    private lateinit var dummyMapView: MapView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        // SplashActivity needs no layout.
-        // Just start main activity and finish.
+        // SplashActivity needs no layout,
+        // because background is set in the theme.
+
+        // This is needed to initialize Google Maps during splash screen,
+        // so that there is no blank screen while starting main activity.
+        dummyMapView = MapView(this)
+        dummyMapView.onCreate(savedInstanceState)
 
         startActivity<PermissionsActivity>()
         finish()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        dummyMapView.onDestroy()
     }
 }
