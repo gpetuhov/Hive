d9c7d9ad80558c7de73824689e33be7ca8d69c63
Gennady Petukhov <gpetukhov@phoenixit.ru>
2018-10-17 10:51:29 +0300

    Request location updates

--- app/src/main/java/com/gpetuhov/android/hive/ui/activity/MainActivity.kt
+++ app/src/main/java/com/gpetuhov/android/hive/ui/activity/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.gpetuhov.android.hive.ui.activity
 
+import android.content.IntentSender
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
@@ -7,16 +8,23 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.gms.common.ConnectionResult
 import com.google.android.gms.common.GoogleApiAvailability
+import com.google.android.gms.common.api.ResolvableApiException
+import com.google.android.gms.location.*
+import com.google.android.gms.tasks.Task
 import com.gpetuhov.android.hive.R
 import com.gpetuhov.android.hive.util.checkPermissions
 import com.pawegio.kandroid.startActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import org.jetbrains.anko.longToast
+import timber.log.Timber
 
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var navController: NavController
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private lateinit var locationCallback: LocationCallback
+    private lateinit var locationRequest: LocationRequest
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,18 +35,43 @@ class MainActivity : AppCompatActivity() {
 
         // Tie NavHostFragment to bottom navigation bar
         navigation_view.setupWithNavController(navController)
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
+        locationCallback = object : LocationCallback() {
+            override fun onLocationResult(locationResult: LocationResult?) {
+                locationResult ?: return
+                for (location in locationResult.locations) {
+                    // TODO: do something
+                    // TODO: why is there a list of locations???
+                    // Update UI with location data
+                    // ...
+                    Timber.tag("Location").d("${location.latitude}, ${location.longitude}")
+                }
+            }
+        }
+
+        createLocationRequest()
     }
 
     override fun onSupportNavigateUp() = navController.navigateUp()
 
     override fun onResume() {
         super.onResume()
+
         if (checkPlayServices()) {
             if (!checkPermissions(this)) {
                 startActivity<PermissionsActivity>()
                 finish()
             }
         }
+
+        startLocationUpdates()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        stopLocationUpdates()
     }
 
     // Check if Google Play Services installed
@@ -58,5 +91,59 @@ class MainActivity : AppCompatActivity() {
         }
 
         return servicesAvailable
+    }
+
+    private fun createLocationRequest() {
+        locationRequest = LocationRequest().apply {
+            interval = 10000
+            fastestInterval = 5000
+            priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        }
+
+        val builder = LocationSettingsRequest.Builder()
+            .addLocationRequest(locationRequest)
+
+        // TODO: maybe remove this code
+        val client: SettingsClient = LocationServices.getSettingsClient(this)
+        val task: Task<LocationSettingsResponse> = client.checkLocationSettings(builder.build())
+
+        task.addOnSuccessListener { locationSettingsResponse ->
+            // All location settings are satisfied. The client can initialize
+            // location requests here.
+            // ...
+
+            // TODO: do something here
+        }
+
+        task.addOnFailureListener { exception ->
+            if (exception is ResolvableApiException){
+                // TODO: do something here
+
+                // Location settings are not satisfied, but this can be fixed
+                // by showing the user a dialog.
+                try {
+                    // Show the dialog by calling startResolutionForResult(),
+                    // and check the result in onActivityResult().
+                    exception.startResolutionForResult(this@MainActivity,
+                        101)
+                } catch (sendEx: IntentSender.SendIntentException) {
+                    // Ignore the error.
+                }
+            }
+        }
+    }
+
+    private fun startLocationUpdates() {
+        try {
+            fusedLocationClient.requestLocationUpdates(locationRequest,
+                locationCallback,
+                null /* Looper */)
+        } catch (e: SecurityException) {
+            Timber.tag("Location").d("Location permission not granted")
+        }
+    }
+
+    private fun stopLocationUpdates() {
+        fusedLocationClient.removeLocationUpdates(locationCallback)
     }
 }
