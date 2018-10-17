dc4231effcda9db88a0db73287e6ca46dee23e7f
Gennady Petukhov <gpetukhov@phoenixit.ru>
2018-10-17 14:52:51 +0300

    Refactor map initialization from MapFragment to MapManager

--- app/src/main/java/com/gpetuhov/android/hive/managers/MapManager.kt
+++ app/src/main/java/com/gpetuhov/android/hive/managers/MapManager.kt
@@ -2,11 +2,25 @@ package com.gpetuhov.android.hive.managers
 
 import android.app.Activity
 import android.os.Bundle
+import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.MapView
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.model.CameraPosition
+import com.gpetuhov.android.hive.util.Constants.Map.Companion.DEFAULT_LATITUDE
+import com.gpetuhov.android.hive.util.Constants.Map.Companion.DEFAULT_LONGITUDE
+import com.gpetuhov.android.hive.util.Constants.Map.Companion.DEFAULT_ZOOM
+import com.gpetuhov.android.hive.util.Constants.Map.Companion.NO_ZOOM
+import timber.log.Timber
+
 
 class MapManager(locationManagerInput: LocationManager) {
 
+    companion object {
+        private const val TAG = "MapManager"
+    }
+
     private val locationManager = locationManagerInput
+    private lateinit var googleMap: GoogleMap
 
     // Calling this method forces Google Maps (Google Play Services) to load.
     // So afterwards, when the map is really needed, it will load faster.
@@ -14,5 +28,40 @@ class MapManager(locationManagerInput: LocationManager) {
         val dummyMapView = MapView(activity)
         dummyMapView.onCreate(savedInstanceState)
         dummyMapView.onDestroy()
+    }
+
+    fun initMap(map: GoogleMap) {
+        // When the map is ready, get reference to it
+        googleMap = map
+
+        // For showing a move to my location button
+        try {
+            googleMap.isMyLocationEnabled = true
+
+            locationManager.getLastLocation { location ->
+                val zoom = if (location.latitude == DEFAULT_LATITUDE && location.longitude == DEFAULT_LONGITUDE) NO_ZOOM else DEFAULT_ZOOM
+
+                val cameraPosition = CameraPosition.Builder().target(location).zoom(zoom).build()
+                googleMap.moveCamera(CameraUpdateFactory.newCameraPosition(cameraPosition))
+            }
+
+        } catch (e: SecurityException) {
+            Timber.tag(TAG).d("Location permission not granted")
+        }
+
+        // Enable compass (will show on map rotate)
+        googleMap.uiSettings.isCompassEnabled = true
+
+        // Enable zoom buttons
+        googleMap.uiSettings.isZoomControlsEnabled = true
+
+        // For dropping a marker at a point on the Map
+//        val sydney = LatLng(-34.0, 151.0)
+//        googleMap.addMarker(MarkerOptions().position(sydney).title("Marker Title").snippet("Marker Description"))
+
+        // When the map is loaded, do something
+        googleMap.setOnMapLoadedCallback {
+            // TODO: do something
+        }
     }
 }
--- app/src/main/java/com/gpetuhov/android/hive/ui/fragment/MapFragment.kt
+++ app/src/main/java/com/gpetuhov/android/hive/ui/fragment/MapFragment.kt
@@ -5,32 +5,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.MapView
-import com.google.android.gms.maps.OnMapReadyCallback
-import com.google.android.gms.maps.model.CameraPosition
-import com.google.android.gms.maps.model.MarkerOptions
 import com.gpetuhov.android.hive.R
 import com.gpetuhov.android.hive.application.HiveApp
-import com.gpetuhov.android.hive.managers.LocationManager
-import com.gpetuhov.android.hive.util.Constants.Map.Companion.DEFAULT_LATITUDE
-import com.gpetuhov.android.hive.util.Constants.Map.Companion.DEFAULT_LONGITUDE
-import com.gpetuhov.android.hive.util.Constants.Map.Companion.DEFAULT_ZOOM
-import com.gpetuhov.android.hive.util.Constants.Map.Companion.NO_ZOOM
-import timber.log.Timber
+import com.gpetuhov.android.hive.managers.MapManager
 import javax.inject.Inject
 
-class MapFragment : Fragment(), OnMapReadyCallback {
-
-    companion object {
-        private const val TAG = "MapFragment"
-    }
+class MapFragment : Fragment() {
 
     @Inject
-    lateinit var locationManager: LocationManager
+    lateinit var mapManager: MapManager
 
-    private lateinit var googleMap: GoogleMap
     private lateinit var mapView: MapView
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -45,7 +30,7 @@ class MapFragment : Fragment(), OnMapReadyCallback {
         mapView.onCreate(savedInstanceState)
 
         // Asynchronously get reference to the map
-        mapView.getMapAsync(this)
+        mapView.getMapAsync { map -> mapManager.initMap(map) }
 
         return view
     }
@@ -83,40 +68,5 @@ class MapFragment : Fragment(), OnMapReadyCallback {
     override fun onLowMemory() {
         super.onLowMemory()
         mapView.onLowMemory()
-    }
-
-    override fun onMapReady(map: GoogleMap) {
-        // When the map is ready, get reference to it
-        googleMap = map
-
-        // For showing a move to my location button
-        try {
-            googleMap.isMyLocationEnabled = true
-
-            locationManager.getLastLocation { location ->
-                val zoom = if (location.latitude == DEFAULT_LATITUDE && location.longitude == DEFAULT_LONGITUDE) NO_ZOOM else DEFAULT_ZOOM
-
-                val cameraPosition = CameraPosition.Builder().target(location).zoom(zoom).build()
-                googleMap.moveCamera(CameraUpdateFactory.newCameraPosition(cameraPosition))
-            }
-
-        } catch (e: SecurityException) {
-            Timber.tag(TAG).d("Location permission not granted")
-        }
-
-        // Enable compass (will show on map rotate)
-        googleMap.uiSettings.isCompassEnabled = true
-
-        // Enable zoom buttons
-        googleMap.uiSettings.isZoomControlsEnabled = true
-
-        // For dropping a marker at a point on the Map
-//        val sydney = LatLng(-34.0, 151.0)
-//        googleMap.addMarker(MarkerOptions().position(sydney).title("Marker Title").snippet("Marker Description"))
-
-        // When the map is loaded, do something
-        googleMap.setOnMapLoadedCallback {
-            // TODO: do something
-        }
     }
 }
