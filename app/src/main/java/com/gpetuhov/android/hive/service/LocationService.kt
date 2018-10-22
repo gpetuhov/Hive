e5cff6f08ab48a7e1261268152b027dbfebc9f8c
Gennady Petukhov <gpetukhov@phoenixit.ru>
2018-10-22 15:17:55 +0300

    Do not start LocationService in foreground

--- app/src/main/java/com/gpetuhov/android/hive/service/LocationService.kt
+++ app/src/main/java/com/gpetuhov/android/hive/service/LocationService.kt
@@ -1,13 +1,9 @@
 package com.gpetuhov.android.hive.service
 
-import android.app.Notification
-import android.app.Service
 import android.content.Intent
 import android.os.IBinder
-import androidx.core.app.NotificationCompat
 import timber.log.Timber
-import android.app.PendingIntent
-import com.gpetuhov.android.hive.ui.activity.MainActivity
+import android.app.Service
 
 
 // Shares current location
@@ -15,40 +11,16 @@ class LocationService : Service() {
 
     companion object {
         private const val TAG = "LocationService"
-        private const val SERVICE_NOTIFICATION_ID = 100
-    }
-
-    override fun onCreate() {
-        super.onCreate()
-        Timber.tag(TAG).d("onCreate")
-        startForegroundAndShowNotification()
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         Timber.tag(TAG).d("onStartCommand")
+
+        // This is needed for the service to be recreated, if killed by the OS
         return START_STICKY
     }
 
     override fun onBind(intent: Intent?): IBinder? {
         return null
-    }
-
-    private fun startForegroundAndShowNotification() {
-        // This is needed to prevent service from being killed by the OS
-        startForeground(SERVICE_NOTIFICATION_ID, getNotification())
-    }
-
-    private fun getNotification(): Notification? {
-        return NotificationCompat.Builder(this, "Default")
-            .setContentTitle("Notification")
-            .setContentText("Text text text")
-            .setContentIntent(getMainActivityPendingIntent())
-            .build()
-    }
-
-    private fun getMainActivityPendingIntent(): PendingIntent {
-        val updateIntent = Intent(this, MainActivity::class.java)
-        updateIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_SINGLE_TOP)
-        return PendingIntent.getActivity(this, 0, updateIntent, 0)
     }
 }
