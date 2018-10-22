ba6d7f790988e4de2fb63359128e3cfb39871a04
Gennady Petukhov <gpetukhov@phoenixit.ru>
2018-10-22 16:31:34 +0300

    Remove Timber debug tree

--- app/src/main/java/com/gpetuhov/android/hive/application/HiveApp.kt
+++ app/src/main/java/com/gpetuhov/android/hive/application/HiveApp.kt
@@ -1,7 +1,6 @@
 package com.gpetuhov.android.hive.application
 
 import androidx.multidex.MultiDexApplication
-import com.gpetuhov.android.hive.BuildConfig
 import com.gpetuhov.android.hive.application.dagger.components.AppComponent
 import com.gpetuhov.android.hive.application.dagger.components.DaggerAppComponent
 import com.gpetuhov.android.hive.util.CrashlyticsTree
@@ -16,11 +15,6 @@ class HiveApp : MultiDexApplication() {
 
     override fun onCreate() {
         super.onCreate()
-
-        // Write logs to consonle in debug only
-        if (BuildConfig.DEBUG) {
-            Timber.plant(Timber.DebugTree())
-        }
 
         // Write logs to Crashlytics both in debug and release
         Timber.plant(CrashlyticsTree())
