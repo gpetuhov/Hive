6308c5ca65a8de1ca291a773fbac653b7f920cd2
Gennady Petukhov <gpetukhov@phoenixit.ru>
2018-10-23 12:49:00 +0300

    Disable sound for location sharing notifications

--- app/src/main/java/com/gpetuhov/android/hive/managers/NotificationManager.kt
+++ app/src/main/java/com/gpetuhov/android/hive/managers/NotificationManager.kt
@@ -5,7 +5,6 @@ import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
-import android.content.Context.NOTIFICATION_SERVICE
 import android.content.Intent
 import android.os.Build
 import androidx.core.app.NotificationCompat
@@ -19,7 +18,7 @@ class NotificationManager {
 
     companion object {
         const val LOCATION_SHARING_NOTIFICATION_ID = 1001
-        private const val DEFAULT_CHANNEL = "default_channel"
+        private const val LOCATION_SHARING_CHANNEL = "location_sharing_channel"
     }
 
     @Inject lateinit var context: Context
@@ -34,7 +33,7 @@ class NotificationManager {
         intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_SINGLE_TOP
         val pendingIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_CANCEL_CURRENT)
 
-        val builder = NotificationCompat.Builder(context, DEFAULT_CHANNEL)
+        val builder = NotificationCompat.Builder(context, LOCATION_SHARING_CHANNEL)
             .setContentTitle(context.getString(R.string.app_name))
             .setContentText(context.getString(R.string.location_sharing_enabled))
             .setSmallIcon(R.drawable.android_round)
@@ -44,17 +43,19 @@ class NotificationManager {
     }
 
     private fun createDefaultNotificationChannel() {
+        // Notification channels are needed since Android Oreo
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             // Create the NotificationChannel
-            val name = "Default"
-            val descriptionText = "Default channel"
-            val importance = android.app.NotificationManager.IMPORTANCE_DEFAULT
-            val mChannel = NotificationChannel(DEFAULT_CHANNEL, name, importance)
-            mChannel.description = descriptionText
+            val name = context.getString(R.string.location_sharing_channel_name)
+            val descriptionText = context.getString(R.string.location_sharing_channel_description)
+            val importance = android.app.NotificationManager.IMPORTANCE_LOW
+            val channel = NotificationChannel(LOCATION_SHARING_CHANNEL, name, importance)
+            channel.description = descriptionText
+
             // Register the channel with the system; you can't change the importance
             // or other notification behaviors after this
             val notificationManager = ContextCompat.getSystemService(context, NotificationManager::class.java) as NotificationManager
-            notificationManager.createNotificationChannel(mChannel)
+            notificationManager.createNotificationChannel(channel)
         }
     }
 }
