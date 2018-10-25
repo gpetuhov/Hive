013a55c55c86fd76725b8dedfc54d40fdeb85604
Gennady Petukhov <gpetukhov@phoenixit.ru>
2018-10-25 18:30:58 +0300

    Change ProfileFragmentView strategy

--- app/src/main/java/com/gpetuhov/android/hive/presentation/view/ProfileFragmentView.kt
+++ app/src/main/java/com/gpetuhov/android/hive/presentation/view/ProfileFragmentView.kt
@@ -1,12 +1,11 @@
 package com.gpetuhov.android.hive.presentation.view
 
 import com.arellomobile.mvp.MvpView
-import com.arellomobile.mvp.viewstate.strategy.SingleStateStrategy
+import com.arellomobile.mvp.viewstate.strategy.SkipStrategy
 import com.arellomobile.mvp.viewstate.strategy.StateStrategyType
 
 // This is the interface, that ProfileFragment must implement.
 // Presenter calls methods of this interface.
-//@StateStrategyType(SingleStateStrategy::class)
 interface ProfileFragmentView : MvpView {
     fun showSignOutDialog()
     fun dismissSignOutDialog()
@@ -14,5 +13,9 @@ interface ProfileFragmentView : MvpView {
     fun dismissDeleteUserDialog()
     fun showUsernameDialog()
     fun dismissUsernameDialog()
+
+    // We don't need to keep this command in the queue,
+    // just run this once on error and don't repeat on view being recreated.
+    @StateStrategyType(SkipStrategy::class)
     fun onSaveUsernameError()
 }
