c9be4b894b79e39b14efed08935a7f317e61937d
Gennady Petukhov <gpetukhov@phoenixit.ru>
2018-10-29 12:30:46 +0300

    Add UserTest

---
+++ app/src/test/java/com/gpetuhov/android/hive/UserTest.kt
@@ -0,0 +1,32 @@
+package com.gpetuhov.android.hive
+
+import com.google.android.gms.maps.model.LatLng
+import com.gpetuhov.android.hive.domain.model.User
+import com.gpetuhov.android.hive.util.Constants
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Test
+
+class UserTest {
+
+    private lateinit var user: User
+
+    @Before
+    fun initUser() {
+        user = User(
+            "342gg34g2",
+            "John",
+            "",
+            "mail@mail.com",
+            false,
+            LatLng(Constants.Map.DEFAULT_LATITUDE, Constants.Map.DEFAULT_LONGITUDE)
+        )
+    }
+
+    @Test
+    fun emptyUsername() {
+        assertEquals(false, user.hasUsername)
+        user.username = "john7"
+        assertEquals(true, user.hasUsername)
+    }
+}
