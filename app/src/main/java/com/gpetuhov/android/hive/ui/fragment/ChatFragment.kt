ed4470df39dc82e8495dea07aba665b6e40d5639
Gennady Petukhov <gpetukhov@phoenixit.ru>
2018-11-06 14:03:06 +0300

    Implement navigate back from chat fragment

--- app/src/main/java/com/gpetuhov/android/hive/ui/fragment/ChatFragment.kt
+++ app/src/main/java/com/gpetuhov/android/hive/ui/fragment/ChatFragment.kt
@@ -4,16 +4,23 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.gpetuhov.android.hive.R
+import com.gpetuhov.android.hive.databinding.FragmentChatBinding
 import kotlinx.android.synthetic.main.fragment_chat.*
 
 class ChatFragment : Fragment() {
 
+    private var binding: FragmentChatBinding? = null
     private var uid = ""
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_chat, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_chat, container, false)
+        binding?.handler = this
+
+        return binding?.root
     }
 
     override fun onResume() {
@@ -21,5 +28,11 @@ class ChatFragment : Fragment() {
         uid = ChatFragmentArgs.fromBundle(arguments).uid
         val name = ChatFragmentArgs.fromBundle(arguments).name
         chat_user_name_text.text = name
+    }
+
+    // === Public methods ===
+
+    fun navigateUp() {
+        findNavController().navigateUp()
     }
 }
