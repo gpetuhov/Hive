b6f4836721fddc05d5493c262ffc66dca0885eb7
Gennady Petukhov <gpetukhov@phoenixit.ru>
2018-11-06 15:10:51 +0300

    Add message test

---
+++ app/src/test/java/com/gpetuhov/android/hive/models/MessageTest.kt
@@ -0,0 +1,30 @@
+package com.gpetuhov.android.hive.models
+
+import com.gpetuhov.android.hive.domain.model.Message
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Test
+
+class MessageTest {
+
+    companion object {
+        const val DUMMY_SENDER_UID = "9j82qho"
+    }
+
+    private lateinit var message: Message
+
+    @Before
+    fun initUser() {
+        message = Message(
+            senderUid = DUMMY_SENDER_UID,
+            timestamp = System.currentTimeMillis(),
+            text = "Message text"
+        )
+    }
+
+    @Test
+    fun isFromUser() {
+        assertEquals(true, message.isFromUser(DUMMY_SENDER_UID))
+        assertEquals(false, message.isFromUser("563h89ju"))
+    }
+}
