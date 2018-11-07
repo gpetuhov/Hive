cbcaaff54c4bc401b4116036d6f3aede19e7df5e
Gennady Petukhov <gpetukhov@phoenixit.ru>
2018-11-07 15:28:12 +0300

    Use DataBinding to show message

--- app/src/main/java/com/gpetuhov/android/hive/ui/adapter/MessagesAdapter.kt
+++ app/src/main/java/com/gpetuhov/android/hive/ui/adapter/MessagesAdapter.kt
@@ -1,15 +1,12 @@
 package com.gpetuhov.android.hive.ui.adapter
 
-import android.view.Gravity
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.gpetuhov.android.hive.R
 import com.gpetuhov.android.hive.databinding.ItemMessageBinding
 import com.gpetuhov.android.hive.domain.model.Message
-import kotlinx.android.synthetic.main.item_message.view.*
 
 class MessagesAdapter : RecyclerView.Adapter<MessagesAdapter.MessageViewHolder>() {
 
@@ -37,21 +34,10 @@ class MessagesAdapter : RecyclerView.Adapter<MessagesAdapter.MessageViewHolder>(
 
     class MessageViewHolder(itemBinding: ItemMessageBinding) : RecyclerView.ViewHolder(itemBinding.root) {
 
-        private var view = itemBinding.root
         private var binding: ItemMessageBinding = itemBinding
 
         fun bindMessage(message: Message) {
             binding.message = message
-
-            view.apply {
-                // Set message layout depending on message sender (current user or not)
-                message_root_layout.gravity = if (message.isFromCurrentUser) Gravity.END else Gravity.START
-                message_left_space.visibility = if (message.isFromCurrentUser) View.VISIBLE else View.GONE
-                message_right_space.visibility = if (message.isFromCurrentUser) View.GONE else View.VISIBLE
-                item_message_wrapper.setBackgroundResource(
-                    if (message.isFromCurrentUser) R.drawable.message_background_current_user else R.drawable.message_background
-                )
-            }
         }
     }
 }
