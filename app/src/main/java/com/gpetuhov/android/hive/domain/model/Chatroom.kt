28a8125dc2da28a567bd73634e7ad337366529aa
Gennady Petukhov <gpetukhov@phoenixit.ru>
2018-11-08 13:14:40 +0300

    Add Chatroom model

---
+++ app/src/main/java/com/gpetuhov/android/hive/domain/model/Chatroom.kt
@@ -0,0 +1,9 @@
+package com.gpetuhov.android.hive.domain.model
+
+data class Chatroom (
+    var chatroomUid: String,
+    var secondUserUid: String,    // Uid of the second user in the chat (not the current user)
+    var lastMessageText: String,
+    var lastMessageTimestamp: Long
+) {
+}
