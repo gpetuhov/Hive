4bdec27d4fac374e0dec7987e8d0eee0ae20ddad
Gennady Petukhov <gpetukhov@phoenixit.ru>
2018-11-08 13:05:22 +0300

    Rename MessagesFragment into ChatroomsFragment

--- app/src/main/java/com/gpetuhov/android/hive/ui/fragment/ChatroomsFragment.kt
+++ app/src/main/java/com/gpetuhov/android/hive/ui/fragment/ChatroomsFragment.kt
@@ -7,9 +7,9 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.gpetuhov.android.hive.R
 
-class MessagesFragment : Fragment() {
+class ChatroomsFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_messages, container, false)
+        return inflater.inflate(R.layout.fragment_chatrooms, container, false)
     }
 }
