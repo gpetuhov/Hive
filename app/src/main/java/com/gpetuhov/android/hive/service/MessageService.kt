266de814792ec364896abe2edd4b80522816b9a3
Gennady Petukhov <gpetukhov@phoenixit.ru>
2018-11-15 14:14:27 +0300

    Get new FCM token

--- app/src/main/java/com/gpetuhov/android/hive/service/MessageService.kt
+++ app/src/main/java/com/gpetuhov/android/hive/service/MessageService.kt
@@ -9,16 +9,31 @@ class MessageService : FirebaseMessagingService() {
     // Called when message is received.
     override fun onMessageReceived(remoteMessage: RemoteMessage?) {
 
-        // This method is called only if the message is received, when the app is in the FOREGROUND.
-        // When the app is in the background, a standard notification
-        // is displayed in the system tray and onMessageReceived is NOT called.
-        // If the message, received when the app is in the background,
-        // contains data payload, it can be retrieved
-        // from the intent launched when the user taps on the notification.
+        // There are two types of messages data messages and notification messages. Data messages
+        // are handled
+        // here in onMessageReceived whether the app is in the foreground or background. Data
+        // messages are the type
+        // traditionally used with GCM. Notification messages are only received here in
+        // onMessageReceived when the app
+        // is in the foreground. When the app is in the background an automatically generated
+        // notification is displayed.
+        // When the user taps on the notification they are returned to the app. Messages
+        // containing both notification
+        // and data payloads are treated as notification messages. The Firebase console always
+        // sends notification
+        // messages. For more see: https://firebase.google.com/docs/cloud-messaging/concept-options
 
         // Get text from received message
         val messageText = remoteMessage?.notification?.body
+        val messageData = remoteMessage?.data.toString()
 
-        Timber.tag("MessageService").d("Received message: $messageText")
+        Timber.tag("MessageService").d("messageText = $messageText")
+        Timber.tag("MessageService").d("messageData = $messageData")
+    }
+
+    override fun onNewToken(token: String?) {
+        // This one is called when new FCM token is received
+
+        Timber.tag("MessageService").d("Token = $token")
     }
 }
