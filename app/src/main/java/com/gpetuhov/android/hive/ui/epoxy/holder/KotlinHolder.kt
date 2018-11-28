3b8501894572d406449e8565100e3e0894470f60
Gennady Petukhov <gpetukhov@phoenixit.ru>
2018-11-28 13:39:21 +0300

    Add Epoxy

---
+++ app/src/main/java/com/gpetuhov/android/hive/ui/epoxy/holder/KotlinHolder.kt
@@ -0,0 +1,46 @@
+package com.gpetuhov.android.hive.ui.epoxy.holder
+
+import android.view.View
+import com.airbnb.epoxy.EpoxyHolder
+import kotlin.properties.ReadOnlyProperty
+import kotlin.reflect.KProperty
+
+// Taken from Epoxy sample app
+// https://github.com/airbnb/epoxy
+
+/**
+ * A pattern for easier view binding with an [EpoxyHolder]
+ */
+abstract class KotlinHolder : EpoxyHolder() {
+    private lateinit var view: View
+
+    override fun bindView(itemView: View) {
+        view = itemView
+    }
+
+    protected fun <V : View> bind(id: Int): ReadOnlyProperty<KotlinHolder, V> =
+        Lazy { holder: KotlinHolder, prop ->
+            holder.view.findViewById(id) as V?
+                ?: throw IllegalStateException("View ID $id for '${prop.name}' not found.")
+        }
+
+    /**
+     * Taken from Kotterknife.
+     * https://github.com/JakeWharton/kotterknife
+     */
+    private class Lazy<V>(
+        private val initializer: (KotlinHolder, KProperty<*>) -> V
+    ) : ReadOnlyProperty<KotlinHolder, V> {
+        private object EMPTY
+
+        private var value: Any? = EMPTY
+
+        override fun getValue(thisRef: KotlinHolder, property: KProperty<*>): V {
+            if (value == EMPTY) {
+                value = initializer(thisRef, property)
+            }
+            @Suppress("UNCHECKED_CAST")
+            return value as V
+        }
+    }
+}
