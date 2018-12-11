25d810c50dd749cef4bc15cc6fa3d567e1290af2
Gennady Petukhov <gpetukhov@phoenixit.ru>
2018-12-11 10:27:19 +0300

    Set fullscreen photo height

--- app/src/main/java/com/gpetuhov/android/hive/ui/epoxy/photo/fullscreen/models/PhotoFullscreenItemModel.kt
+++ app/src/main/java/com/gpetuhov/android/hive/ui/epoxy/photo/fullscreen/models/PhotoFullscreenItemModel.kt
@@ -1,5 +1,6 @@
 package com.gpetuhov.android.hive.ui.epoxy.photo.fullscreen.models
 
+import android.content.Context
 import android.widget.ImageView
 import com.airbnb.epoxy.EpoxyAttribute
 import com.airbnb.epoxy.EpoxyModelClass
@@ -8,6 +9,8 @@ import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
 import com.gpetuhov.android.hive.R
 import com.gpetuhov.android.hive.ui.epoxy.holder.KotlinHolder
+import android.util.DisplayMetrics
+import android.view.WindowManager
 
 @EpoxyModelClass(layout = R.layout.photo_fullscreen_item_view)
 abstract class PhotoFullscreenItemModel : EpoxyModelWithHolder<PhotoFullscreenItemHolder>() {
@@ -15,12 +18,36 @@ abstract class PhotoFullscreenItemModel : EpoxyModelWithHolder<PhotoFullscreenIt
     @EpoxyAttribute lateinit var photoUrl: String
 
     override fun bind(holder: PhotoFullscreenItemHolder) {
+        setImageHeight(holder.photo)
+
         if (photoUrl != "") {
             Glide.with(holder.photo.context)
                 .load(photoUrl)
                 .apply(RequestOptions.centerInsideTransform())
                 .into(holder.photo)
         }
+    }
+
+    private fun setImageHeight(imageView: ImageView) {
+        val imageHeight = getScreenHeight(imageView) - getStatusBarHeight(imageView)
+        imageView.layoutParams.height = imageHeight
+    }
+
+    private fun getScreenHeight(imageView: ImageView): Int {
+        val wm = imageView.context.getSystemService(Context.WINDOW_SERVICE) as WindowManager?
+        val display = wm?.defaultDisplay
+        val metrics = DisplayMetrics()
+        display?.getMetrics(metrics)
+        return metrics.heightPixels
+    }
+
+    private fun getStatusBarHeight(imageView: ImageView): Int {
+        var statusBarHeight = 0
+        val resourceId = imageView.resources.getIdentifier("status_bar_height", "dimen", "android")
+        if (resourceId > 0) {
+            statusBarHeight = imageView.resources.getDimensionPixelSize(resourceId)
+        }
+        return statusBarHeight
     }
 }
 
