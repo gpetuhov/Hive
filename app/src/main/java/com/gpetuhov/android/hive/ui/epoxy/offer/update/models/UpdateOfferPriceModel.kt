19dec03c6d6ec67739fbe68f745cb9b0ce1b4d24
Gennady Petukhov <gpetukhov@phoenixit.ru>
2018-12-12 18:29:20 +0300

    Replace offer free Switch with CheckBox

--- app/src/main/java/com/gpetuhov/android/hive/ui/epoxy/offer/update/models/UpdateOfferPriceModel.kt
+++ app/src/main/java/com/gpetuhov/android/hive/ui/epoxy/offer/update/models/UpdateOfferPriceModel.kt
@@ -1,8 +1,8 @@
 package com.gpetuhov.android.hive.ui.epoxy.offer.update.models
 
 import android.view.View
+import android.widget.CheckBox
 import android.widget.TextView
-import androidx.appcompat.widget.SwitchCompat
 import com.airbnb.epoxy.EpoxyAttribute
 import com.airbnb.epoxy.EpoxyModelClass
 import com.airbnb.epoxy.EpoxyModelWithHolder
@@ -20,7 +20,7 @@ abstract class UpdateOfferPriceModel : EpoxyModelWithHolder<UpdateOfferPriceHold
 
     override fun bind(holder: UpdateOfferPriceHolder) {
         holder.free.isChecked = free
-        holder.free.setOnClickListener { view -> onFreeClick((view as SwitchCompat).isChecked) }
+        holder.free.setOnClickListener { view -> onFreeClick((view as CheckBox).isChecked) }
 
         holder.priceWrapper.visibility = if (free) View.GONE else View.VISIBLE
         holder.priceWrapper.setOnClickListener { onPriceClick() }
@@ -29,7 +29,7 @@ abstract class UpdateOfferPriceModel : EpoxyModelWithHolder<UpdateOfferPriceHold
 }
 
 class UpdateOfferPriceHolder : KotlinHolder() {
-    val free by bind<SwitchCompat>(R.id.update_offer_free)
+    val free by bind<CheckBox>(R.id.update_offer_free)
     val priceWrapper by bind<View>(R.id.update_offer_price_wrapper)
     val price by bind<TextView>(R.id.update_offer_price)
 }
