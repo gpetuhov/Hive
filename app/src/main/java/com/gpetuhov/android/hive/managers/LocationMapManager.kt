48584363b0b48b2e9c6fc76c0dfc94683f184b36
Gennady Petukhov <gpetukhov@phoenixit.ru>
2018-12-14 18:22:26 +0300

    Add BaseMapManager

--- app/src/main/java/com/gpetuhov/android/hive/managers/LocationMapManager.kt
+++ app/src/main/java/com/gpetuhov/android/hive/managers/LocationMapManager.kt
@@ -2,43 +2,13 @@ package com.gpetuhov.android.hive.managers
 
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.model.LatLng
-import com.gpetuhov.android.hive.util.Constants
+import com.gpetuhov.android.hive.managers.base.BaseMapManager
 import com.gpetuhov.android.hive.util.addSingleMarker
 import com.gpetuhov.android.hive.util.moveCamera
-import timber.log.Timber
 
-class LocationMapManager {
+class LocationMapManager : BaseMapManager() {
 
-    private lateinit var googleMap: GoogleMap
-
-    fun initMap(map: GoogleMap) {
-        // When the map is ready, get reference to it
-        googleMap = map
-
-        try {
-            // Show my location (blue point)
-            googleMap.isMyLocationEnabled = true
-
-        } catch (e: SecurityException) {
-            Timber.tag("LocationFragment").d("Location permission not granted")
-        }
-
-        // Enable compass (will show on map rotate)
-        googleMap.uiSettings.isCompassEnabled = true
-
-        // Enable my location button
-        googleMap.uiSettings.isMyLocationButtonEnabled = true
-
-        // Enable zoom buttons
-        googleMap.uiSettings.isZoomControlsEnabled = true
-
-        // Enable toolbar that opens Google Maps App
-        googleMap.uiSettings.isMapToolbarEnabled = true
-
-        // Set minimum and maximum zoom
-        googleMap.setMinZoomPreference(Constants.Map.MIN_ZOOM)
-        googleMap.setMaxZoomPreference(Constants.Map.MAX_ZOOM)
-    }
+    fun initMap(map: GoogleMap) = super.initMap(map, true)
 
     fun updateLocation(location: LatLng) {
         googleMap.moveCamera(location)
---
+++ app/src/main/java/com/gpetuhov/android/hive/managers/base/BaseMapManager.kt
@@ -0,0 +1,41 @@
+package com.gpetuhov.android.hive.managers.base
+
+import com.google.android.gms.maps.GoogleMap
+import com.gpetuhov.android.hive.util.Constants
+import timber.log.Timber
+
+open class BaseMapManager {
+
+    protected lateinit var googleMap: GoogleMap
+
+    // === Protected methods ===
+
+    fun initMap(map: GoogleMap, controlsEnabled: Boolean) {
+        // When the map is ready, get reference to it
+        googleMap = map
+
+        try {
+            // Show my location (blue point)
+            googleMap.isMyLocationEnabled = true
+
+        } catch (e: SecurityException) {
+            Timber.tag("LocationFragment").d("Location permission not granted")
+        }
+
+        // Enable compass (will show on map rotate)
+        googleMap.uiSettings.isCompassEnabled = true
+
+        // Enable my location button
+        googleMap.uiSettings.isMyLocationButtonEnabled = controlsEnabled
+
+        // Enable zoom buttons
+        googleMap.uiSettings.isZoomControlsEnabled = controlsEnabled
+
+        // Enable toolbar that opens Google Maps App
+        googleMap.uiSettings.isMapToolbarEnabled = controlsEnabled
+
+        // Set minimum and maximum zoom
+        googleMap.setMinZoomPreference(Constants.Map.MIN_ZOOM)
+        googleMap.setMaxZoomPreference(Constants.Map.MAX_ZOOM)
+    }
+}
