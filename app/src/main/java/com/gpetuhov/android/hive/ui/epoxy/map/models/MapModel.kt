3f471c4ec70f35bd0bce521603309fabab460b00
Gennady Petukhov <gpetukhov@phoenixit.ru>
2018-12-14 13:06:58 +0300

    Add MapView to MapModel

--- app/src/main/java/com/gpetuhov/android/hive/ui/epoxy/map/models/MapModel.kt
+++ app/src/main/java/com/gpetuhov/android/hive/ui/epoxy/map/models/MapModel.kt
@@ -2,6 +2,7 @@ package com.gpetuhov.android.hive.ui.epoxy.map.models
 
 import com.airbnb.epoxy.EpoxyModelClass
 import com.airbnb.epoxy.EpoxyModelWithHolder
+import com.google.android.gms.maps.MapView
 import com.gpetuhov.android.hive.R
 import com.gpetuhov.android.hive.ui.epoxy.base.KotlinHolder
 
@@ -9,8 +10,18 @@ import com.gpetuhov.android.hive.ui.epoxy.base.KotlinHolder
 abstract class MapModel : EpoxyModelWithHolder<MapHolder>() {
 
     override fun bind(holder: MapHolder) {
+        holder.mapView.onCreate(null)
+
+        holder.mapView.getMapAsync { googleMap ->
+            googleMap.uiSettings.isMapToolbarEnabled = false
+
+            googleMap.setOnMapClickListener {
+                // TODO: implement this
+            }
+        }
     }
 }
 
 class MapHolder : KotlinHolder() {
+    val mapView by bind<MapView>(R.id.user_offer_map_view)
 }
