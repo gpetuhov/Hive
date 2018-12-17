c2d80fe46cc0c5c9148624c0efc7333eb378feec
Gennady Petukhov <gpetukhov@phoenixit.ru>
2018-12-17 18:49:43 +0300

    Show hide favorite users empty view

--- app/src/main/java/com/gpetuhov/android/hive/ui/adapter/FavoritesAdapter.kt
+++ app/src/main/java/com/gpetuhov/android/hive/ui/adapter/FavoritesAdapter.kt
@@ -3,26 +3,16 @@ package com.gpetuhov.android.hive.ui.adapter
 import android.content.Context
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.FragmentPagerAdapter
+import androidx.fragment.app.FragmentStatePagerAdapter
 import com.gpetuhov.android.hive.R
 import com.gpetuhov.android.hive.ui.fragment.FavoriteOffersFragment
 import com.gpetuhov.android.hive.ui.fragment.FavoriteUsersFragment
 
-class FavoritesAdapter(private val context: Context?, fm: FragmentManager?) : FragmentPagerAdapter(fm) {
+class FavoritesAdapter(private val context: Context?, fm: FragmentManager?) : FragmentStatePagerAdapter(fm) {
 
-    override fun getItem(position: Int): Fragment = if (position == 0) {
-        FavoriteUsersFragment()
-    } else {
-        FavoriteOffersFragment()
-    }
+    override fun getItem(position: Int): Fragment = if (position == 0) FavoriteUsersFragment() else FavoriteOffersFragment()
 
-    override fun getCount(): Int {
-        return 2
-    }
+    override fun getCount() = 2
 
-    override fun getPageTitle(position: Int): CharSequence? = if (position == 0) {
-        context?.getString(R.string.users)
-    } else {
-        context?.getString(R.string.offers)
-    }
+    override fun getPageTitle(position: Int) = if (position == 0) context?.getString(R.string.users) else context?.getString(R.string.offers)
 }
--- app/src/main/java/com/gpetuhov/android/hive/ui/fragment/FavoriteUsersFragment.kt
+++ app/src/main/java/com/gpetuhov/android/hive/ui/fragment/FavoriteUsersFragment.kt
@@ -4,12 +4,28 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import com.gpetuhov.android.hive.R
+import com.gpetuhov.android.hive.databinding.FragmentFavoriteUsersBinding
+import com.gpetuhov.android.hive.domain.model.Favorite
 import com.gpetuhov.android.hive.ui.fragment.base.BaseFragment
+import com.gpetuhov.android.hive.ui.viewmodel.FavoritesViewModel
 
 class FavoriteUsersFragment : BaseFragment() {
 
+    private var binding: FragmentFavoriteUsersBinding? = null
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_favorite_users, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_favorite_users, container, false)
+
+        val viewModel = ViewModelProviders.of(this).get(FavoritesViewModel::class.java)
+        viewModel.favorites.observe(this, Observer<MutableList<Favorite>> { favoritesList ->
+            val favoriteUsersList = favoritesList.filter { !it.isOffer() }
+            binding?.favoriteUsersListEmpty = favoriteUsersList.isEmpty()
+        })
+
+        return binding?.root
     }
 }
