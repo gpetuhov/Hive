9d067420aac28b229740e849dc20c5e8f4991bba
Gennady Petukhov <gpetukhov@phoenixit.ru>
2018-12-17 18:55:26 +0300

    Show hide favorite offers empty view

--- app/src/main/java/com/gpetuhov/android/hive/ui/fragment/FavoriteOffersFragment.kt
+++ app/src/main/java/com/gpetuhov/android/hive/ui/fragment/FavoriteOffersFragment.kt
@@ -4,12 +4,28 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import com.gpetuhov.android.hive.R
+import com.gpetuhov.android.hive.databinding.FragmentFavoriteOffersBinding
+import com.gpetuhov.android.hive.domain.model.Favorite
 import com.gpetuhov.android.hive.ui.fragment.base.BaseFragment
+import com.gpetuhov.android.hive.ui.viewmodel.FavoritesViewModel
 
 class FavoriteOffersFragment : BaseFragment()  {
 
+    private var binding: FragmentFavoriteOffersBinding? = null
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_favorite_offers, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_favorite_offers, container, false)
+
+        val viewModel = ViewModelProviders.of(this).get(FavoritesViewModel::class.java)
+        viewModel.favorites.observe(this, Observer<MutableList<Favorite>> { favoritesList ->
+            val favoriteOffersList = favoritesList.filter { it.isOffer() }
+            binding?.favoriteOffersListEmpty = favoriteOffersList.isEmpty()
+        })
+
+        return binding?.root
     }
 }
