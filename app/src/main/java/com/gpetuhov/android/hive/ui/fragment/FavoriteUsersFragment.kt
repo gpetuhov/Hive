0a20982da02cce8e28921b711478d4cd25f4724f
Gennady Petukhov <gpetukhov@phoenixit.ru>
2018-12-17 17:46:10 +0300

    Add no favorite users and offers empty views

--- app/src/main/java/com/gpetuhov/android/hive/ui/fragment/FavoriteUsersFragment.kt
+++ app/src/main/java/com/gpetuhov/android/hive/ui/fragment/FavoriteUsersFragment.kt
@@ -1,6 +1,15 @@
 package com.gpetuhov.android.hive.ui.fragment
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.gpetuhov.android.hive.R
 import com.gpetuhov.android.hive.ui.fragment.base.BaseFragment
 
 class FavoriteUsersFragment : BaseFragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_favorite_users, container, false)
+    }
 }
