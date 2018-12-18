3c42887494651bfc78df563340e22be216a9cd9d
Gennady Petukhov <gpetukhov@phoenixit.ru>
2018-12-18 12:21:03 +0300

    Add FavoriteOffersFragmentPresenter

--- app/src/main/java/com/gpetuhov/android/hive/ui/fragment/FavoriteOffersFragment.kt
+++ app/src/main/java/com/gpetuhov/android/hive/ui/fragment/FavoriteOffersFragment.kt
@@ -7,13 +7,18 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import com.arellomobile.mvp.presenter.InjectPresenter
 import com.gpetuhov.android.hive.R
 import com.gpetuhov.android.hive.databinding.FragmentFavoriteOffersBinding
 import com.gpetuhov.android.hive.domain.model.Favorite
+import com.gpetuhov.android.hive.presentation.presenter.FavoriteOffersFragmentPresenter
+import com.gpetuhov.android.hive.presentation.view.FavoriteOffersFragmentView
 import com.gpetuhov.android.hive.ui.fragment.base.BaseFragment
 import com.gpetuhov.android.hive.ui.viewmodel.FavoritesViewModel
 
-class FavoriteOffersFragment : BaseFragment()  {
+class FavoriteOffersFragment : BaseFragment(), FavoriteOffersFragmentView {
+
+    @InjectPresenter lateinit var presenter: FavoriteOffersFragmentPresenter
 
     private var binding: FragmentFavoriteOffersBinding? = null
 
