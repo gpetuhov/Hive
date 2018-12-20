2cf4783e42cc2e68a09703dd57771f7b5ed0b537
Gennady Petukhov <gpetukhov@phoenixit.ru>
2018-12-20 13:16:18 +0300

    Refactor ImageViewExtensions into ViewUtils

--- app/src/main/java/com/gpetuhov/android/hive/util/ViewUtils.kt
+++ app/src/main/java/com/gpetuhov/android/hive/util/ViewUtils.kt
@@ -1,6 +1,9 @@
 package com.gpetuhov.android.hive.util
 
 import android.view.View
+import android.widget.ImageView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import com.gpetuhov.android.hive.R
 
 fun getStarResourceId(favorite: Boolean) = if (favorite) R.drawable.ic_star else R.drawable.ic_star_border
@@ -9,4 +12,15 @@ fun getPriceColorId(free: Boolean) = if (free) R.color.md_red_600 else R.color.m
 
 fun View.setVisible(visible: Boolean) {
     visibility = if (visible) View.VISIBLE else View.GONE
+}
+
+fun ImageView.load(photoUrl: String, crop: Boolean) {
+    if (photoUrl != "") {
+        Glide.with(context)
+            .load(photoUrl)
+            .apply(
+                if (crop) RequestOptions.centerCropTransform() else RequestOptions.centerInsideTransform()
+            )
+            .into(this)
+    }
 }
