7f5bb5639b6ee77d22b23cb4e3cd7ecd5b50d103
Gennady Petukhov <gpetukhov@phoenixit.ru>
2018-12-21 10:48:01 +0300

    Add update review fragment layout

--- app/src/main/java/com/gpetuhov/android/hive/ui/fragment/UpdateReviewFragment.kt
+++ app/src/main/java/com/gpetuhov/android/hive/ui/fragment/UpdateReviewFragment.kt
@@ -6,9 +6,9 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.fragment.findNavController
-import com.airbnb.epoxy.EpoxyRecyclerView
 import com.arellomobile.mvp.presenter.InjectPresenter
 import com.gpetuhov.android.hive.R
+import com.gpetuhov.android.hive.databinding.FragmentUpdateReviewBinding
 import com.gpetuhov.android.hive.presentation.presenter.UpdateReviewFragmentPresenter
 import com.gpetuhov.android.hive.presentation.view.UpdateReviewFragmentView
 import com.gpetuhov.android.hive.ui.fragment.base.BaseFragment
@@ -20,8 +20,7 @@ class UpdateReviewFragment : BaseFragment(), UpdateReviewFragmentView {
 
     @InjectPresenter lateinit var presenter: UpdateReviewFragmentPresenter
 
-    //    private var controller: ReviewsListController? = null
-//    private var binding: FragmentReviewsBinding? = null
+    private var binding: FragmentUpdateReviewBinding? = null
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Adjust_pan is needed to prevent activity from being pushed up by the keyboard
@@ -30,32 +29,13 @@ class UpdateReviewFragment : BaseFragment(), UpdateReviewFragmentView {
         hideToolbar()
         showBottomNavigationView()
 
-//        controller = UserFavoriteListController(presenter)
-//        controller?.onRestoreInstanceState(savedInstanceState)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_update_review, container, false)
+        binding?.presenter = presenter
 
-//        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_reviews, container, false)
-//        binding?.presenter = presenter
-//
-//        val reviewsRecyclerView = binding?.root?.findViewById<EpoxyRecyclerView>(R.id.reviews_recycler_view)
-//        reviewsRecyclerView?.adapter = controller?.adapter
-
-//        val viewModel = ViewModelProviders.of(this).get(ReviewsViewModel::class.java)
-//        viewModel.reviews.observe(this, Observer<MutableList<Review>> { reviewsList ->
-//            binding?.reviewsListEmpty = reviewsList.isEmpty()
-//            controller?.changeReviewsList(reviewsList)
-//        })
-
-//        return binding?.root
-
-        return null
+        return binding?.root
     }
 
-//    override fun onSaveInstanceState(outState: Bundle) {
-//        super.onSaveInstanceState(outState)
-//        controller?.onSaveInstanceState(outState)
-//    }
-
-    // === ReviewsFragmentView ===
+    // === UpdateReviewFragmentView ===
 
     override fun navigateUp() {
         findNavController().navigateUp()
