40979717ef8ba8d4c91e2be1328ebed8312bdef0
Gennady Petukhov <gpetukhov@phoenixit.ru>
2019-06-10 17:43:39 +0300

    Add clear filter dialog

--- app/src/main/java/com/gpetuhov/android/hive/ui/fragment/FilterFragment.kt
+++ app/src/main/java/com/gpetuhov/android/hive/ui/fragment/FilterFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.fragment.findNavController
+import com.afollestad.materialdialogs.MaterialDialog
 import com.airbnb.epoxy.EpoxyRecyclerView
 import com.arellomobile.mvp.presenter.InjectPresenter
 import com.gpetuhov.android.hive.R
@@ -23,12 +24,16 @@ class FilterFragment : BaseFragment(), FilterFragmentView {
     private var controller: FilterListController? = null
     private var binding: FragmentFilterBinding? = null
 
+    private var clearFilterDialog: MaterialDialog? = null
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Adjust_pan is needed to prevent activity from being pushed up by the keyboard
         setActivitySoftInputPan()
 
         hideMainHeader()
         hideBottomNavigationView()
+
+        initDialogs()
 
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_filter, container, false)
         binding?.presenter = presenter
@@ -54,15 +59,20 @@ class FilterFragment : BaseFragment(), FilterFragmentView {
         super.onSaveInstanceState(outState)
     }
 
+    override fun onDestroyView() {
+        super.onDestroyView()
+
+        // This is needed to prevent memory leaks.
+        // Here we intentially dismiss dialogs directly, not via the presenter,
+        // so that Moxy command queue doesn't change.
+        dismissDialogs()
+    }
+
     // === PrivacyPolicyFragmentView ===
 
-    override fun showClearFilterDialog() {
-        // TODO
-    }
+    override fun showClearFilterDialog() = clearFilterDialog?.show() ?: Unit
 
-    override fun dismissClearFilterDialog() {
-        // TODO
-    }
+    override fun dismissClearFilterDialog() = clearFilterDialog?.dismiss() ?: Unit
 
     override fun navigateUp() {
         findNavController().navigateUp()
@@ -71,4 +81,20 @@ class FilterFragment : BaseFragment(), FilterFragmentView {
     // === Private methods ===
 
     private fun updateUI() = controller?.requestModelBuild()
+
+    private fun initDialogs() = initClearFilterDialog()
+
+    private fun initClearFilterDialog() {
+        if (context != null) {
+            clearFilterDialog = MaterialDialog(context!!)
+                .title(R.string.clear_filter)
+                .message(R.string.clear_filter_prompt)
+                .noAutoDismiss()
+                .cancelable(false)
+                .positiveButton(R.string.yes) { presenter.clearFilter() }
+                .negativeButton(R.string.no) { presenter.dismissClearFilterDialog() }
+        }
+    }
+
+    private fun dismissDialogs() = dismissClearFilterDialog()
 }
