613be98426998a75517b8995ab736bc5a83ab37e
Gennady Petukhov <gpetukhov@phoenixit.ru>
2019-06-11 11:00:31 +0300

    Refactor FilterTest

--- app/src/test/java/com/gpetuhov/android/hive/models/FilterTest.kt
+++ app/src/test/java/com/gpetuhov/android/hive/models/FilterTest.kt
@@ -39,45 +39,30 @@ class FilterTest {
     @Test
     fun toJson() {
         val filter = Filter()
-        checkToJson(filter, SHOW_USERS_OFFERS_KEY, Filter.SHOW_USERS_OFFERS_ALL) { filter.setShowUsersOffersAll() }
-        checkToJson(filter, SHOW_USERS_OFFERS_KEY, Filter.SHOW_USERS_ONLY) { filter.setShowUsersOnly() }
-        checkToJson(filter, SHOW_USERS_OFFERS_KEY, Filter.SHOW_OFFERS_ONLY) { filter.setShowOffersOnly() }
+        checkToJson(filter, SHOW_USERS_OFFERS_KEY, Filter.SHOW_USERS_OFFERS_ALL) { it.setShowUsersOffersAll() }
+        checkToJson(filter, SHOW_USERS_OFFERS_KEY, Filter.SHOW_USERS_ONLY) { it.setShowUsersOnly() }
+        checkToJson(filter, SHOW_USERS_OFFERS_KEY, Filter.SHOW_OFFERS_ONLY) { it.setShowOffersOnly() }
     }
 
     @Test
     fun fromJson() {
         val filter = Filter()
-
-        checkFromJson(
-            filter,
-            { filter.setShowUsersOffersAll() },
-            { it.isShowUsersOffersAll }
-        )
-
-        checkFromJson(
-            filter,
-            { filter.setShowUsersOnly() },
-            { it.isShowUsersOnly }
-        )
-
-        checkFromJson(
-            filter,
-            { filter.setShowOffersOnly() },
-            { it.isShowOffersOnly }
-        )
+        checkFromJson(filter, { it.setShowUsersOffersAll() }, { it.isShowUsersOffersAll })
+        checkFromJson(filter, { it.setShowUsersOnly() }, { it.isShowUsersOnly })
+        checkFromJson(filter, { it.setShowOffersOnly() }, { it.isShowOffersOnly })
     }
 
     // === Private methods ===
 
-    private fun checkToJson(filter: Filter, expectedJsonKey: String, expectedJsonValue: Any, action: () -> Unit) {
-        action()
+    private fun checkToJson(filter: Filter, expectedJsonKey: String, expectedJsonValue: Any, action: (Filter) -> Unit) {
+        action(filter)
         val json = Filter.toJson(filter)
         val expectedJsonString = "\"$expectedJsonKey\":$expectedJsonValue}"
         assertEquals(true, json.contains(expectedJsonString))
     }
 
-    private fun checkFromJson(sourceFilter: Filter, action: () -> Unit, assertion: (Filter) -> Unit) {
-        action()
+    private fun checkFromJson(sourceFilter: Filter, action: (Filter) -> Unit, assertion: (Filter) -> Unit) {
+        action(sourceFilter)
         val json = Filter.toJson(sourceFilter)
         val resultFilter = Filter.fromJson(json)
         assertEquals(true, resultFilter != null)
