ded812dceea5bd338b1a0d55bdffc4591f4e93c6
Gennady Petukhov <gpetukhov@phoenixit.ru>
2019-06-13 11:59:17 +0300

    Add filter tests

--- app/src/test/java/com/gpetuhov/android/hive/models/FilterTest.kt
+++ app/src/test/java/com/gpetuhov/android/hive/models/FilterTest.kt
@@ -10,6 +10,14 @@ class FilterTest {
         private const val SHOW_USERS_OFFERS_KEY = "showUsersOffers"
         private const val FREE_OFFERS_ONLY_KEY = "freeOffersOnly"
         private const val OFFERS_WITH_REVIEWS_ONLY_KEY = "offersWithReviewsOnly"
+        private const val HAS_PHONE_KEY = "hasPhone"
+        private const val HAS_EMAIL_KEY = "hasEmail"
+        private const val HAS_SKYPE_KEY = "hasSkype"
+        private const val HAS_FACEBOOK_KEY = "hasFacebook"
+        private const val HAS_TWITTER_KEY = "hasTwitter"
+        private const val HAS_INSTAGRAM_KEY = "hasInstagram"
+        private const val HAS_YOUTUBE_KEY = "hasYoutube"
+        private const val HAS_WEBSITE_KEY = "hasWebsite"
     }
 
     @Test
@@ -45,6 +53,14 @@ class FilterTest {
         checkToJson(SHOW_USERS_OFFERS_KEY, Filter.SHOW_OFFERS_ONLY) { it.setShowOffersOnly() }
         checkToJson(FREE_OFFERS_ONLY_KEY, true) { it.isFreeOffersOnly = true }
         checkToJson(OFFERS_WITH_REVIEWS_ONLY_KEY, true) { it.isOffersWithReviewsOnly = true }
+        checkToJson(HAS_PHONE_KEY, true) { it.hasPhone = true }
+        checkToJson(HAS_EMAIL_KEY, true) { it.hasEmail = true }
+        checkToJson(HAS_SKYPE_KEY, true) { it.hasSkype = true }
+        checkToJson(HAS_FACEBOOK_KEY, true) { it.hasFacebook = true }
+        checkToJson(HAS_TWITTER_KEY, true) { it.hasTwitter = true }
+        checkToJson(HAS_INSTAGRAM_KEY, true) { it.hasInstagram = true }
+        checkToJson(HAS_YOUTUBE_KEY, true) { it.hasYoutube = true }
+        checkToJson(HAS_WEBSITE_KEY, true) { it.hasWebsite = true }
     }
 
     @Test
@@ -54,6 +70,14 @@ class FilterTest {
         checkFromJson({ it.setShowOffersOnly() }, { it.isShowOffersOnly })
         checkFromJson({ it.isFreeOffersOnly = true }, { it.isFreeOffersOnly })
         checkFromJson({ it.isOffersWithReviewsOnly = true }, { it.isOffersWithReviewsOnly })
+        checkFromJson({ it.hasPhone = true }, { it.hasPhone })
+        checkFromJson({ it.hasEmail = true }, { it.hasEmail })
+        checkFromJson({ it.hasSkype = true }, { it.hasSkype })
+        checkFromJson({ it.hasFacebook = true }, { it.hasFacebook })
+        checkFromJson({ it.hasTwitter = true }, { it.hasTwitter })
+        checkFromJson({ it.hasInstagram = true }, { it.hasInstagram })
+        checkFromJson({ it.hasYoutube = true }, { it.hasYoutube })
+        checkFromJson({ it.hasWebsite = true }, { it.hasWebsite })
     }
 
     @Test
@@ -62,6 +86,14 @@ class FilterTest {
         checkIsNotDefault { it.setShowUsersOnly() }
         checkIsNotDefault { it.isFreeOffersOnly = true }
         checkIsNotDefault { it.isOffersWithReviewsOnly = true }
+        checkIsNotDefault { it.hasPhone = true }
+        checkIsNotDefault { it.hasEmail = true }
+        checkIsNotDefault { it.hasSkype = true }
+        checkIsNotDefault { it.hasFacebook = true }
+        checkIsNotDefault { it.hasTwitter = true }
+        checkIsNotDefault { it.hasInstagram = true }
+        checkIsNotDefault { it.hasYoutube = true }
+        checkIsNotDefault { it.hasWebsite = true }
     }
 
     // === Private methods ===
