b824b0a69e5cb203195a1e9c52d30f89c21e6480
Gennady Petukhov <gpetukhov@phoenixit.ru>
2019-06-19 17:26:48 +0300

    Refactor ChatFragmentPresenter

--- app/src/main/java/com/gpetuhov/android/hive/presentation/presenter/ChatFragmentPresenter.kt
+++ app/src/main/java/com/gpetuhov/android/hive/presentation/presenter/ChatFragmentPresenter.kt
@@ -1,7 +1,6 @@
 package com.gpetuhov.android.hive.presentation.presenter
 
 import android.view.View
-import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.arellomobile.mvp.InjectViewState
 import com.arellomobile.mvp.MvpPresenter
@@ -9,6 +8,7 @@ import com.gpetuhov.android.hive.application.HiveApp
 import com.gpetuhov.android.hive.domain.interactor.SendMessageInteractor
 import com.gpetuhov.android.hive.domain.repository.Repo
 import com.gpetuhov.android.hive.managers.NotificationManager
+import com.gpetuhov.android.hive.presentation.presenter.base.BaseChatPresenter
 import com.gpetuhov.android.hive.presentation.view.ChatFragmentView
 import com.gpetuhov.android.hive.ui.adapter.MessagesAdapter
 import javax.inject.Inject
@@ -16,26 +16,22 @@ import javax.inject.Inject
 @InjectViewState
 class ChatFragmentPresenter :
     MvpPresenter<ChatFragmentView>(),
+    BaseChatPresenter,
     SendMessageInteractor.Callback,
     MessagesAdapter.Callback {
 
-    companion object {
-        private const val MIN_SCROLL = 1
-        private const val MIN_SCROLL_SUM = 200
-    }
-
     @Inject lateinit var repo: Repo
     @Inject lateinit var notificationManager: NotificationManager
+
+    override var lastScrollPosition: Int = 0
+    override var scrollSum: Int = 0
+    override lateinit var scrollListener: RecyclerView.OnScrollListener
 
     // Two-way data binding is used for this property
     var messageText = ""
 
     var secondUserUid = ""
-    lateinit var scrollListener: RecyclerView.OnScrollListener
     lateinit var layoutChangeListener: View.OnLayoutChangeListener
-
-    private var scrollSum = 0
-    private var lastScrollPosition = 0
 
     private val sendMessageInteractor = SendMessageInteractor(this)
 
@@ -44,6 +40,10 @@ class ChatFragmentPresenter :
         initScrollListener()
         initLayoutChangeListener()
     }
+
+    override fun showScrollDownButton() = viewState.showScrollDownButton()
+
+    override fun hideScrollDownButton() = viewState.hideScrollDownButton()
 
     // === SendMessageInteractor.Callback ===
 
@@ -71,7 +71,7 @@ class ChatFragmentPresenter :
     fun scrollToLastMessage() {
         lastScrollPosition = 0
         viewState.scrollToPosition(0)
-        viewState.hideScrollDownButton()
+        hideScrollDownButton()
     }
 
     fun openChatArchive() = viewState.openChatArchive()
@@ -93,42 +93,6 @@ class ChatFragmentPresenter :
     }
 
     // === Private methods ===
-
-    private fun initScrollListener() {
-        scrollListener = object : RecyclerView.OnScrollListener() {
-            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
-                super.onScrolled(recyclerView, dx, dy)
-
-                // Reset scroll sum if changed scroll direction
-                if (scrollSum < 0 && dy > MIN_SCROLL || scrollSum > 0 && dy < -MIN_SCROLL) scrollSum = 0
-
-                scrollSum += dy
-
-                // Show scroll down button on messages list scroll down for more than MIN_SCROLL_SUM,
-                // hide on scroll up for more than MIN_SCROLL_SUM.
-                if (scrollSum > MIN_SCROLL_SUM) {
-                    viewState.showScrollDownButton()
-                } else if (scrollSum < -MIN_SCROLL_SUM) {
-                    viewState.hideScrollDownButton()
-                }
-            }
-
-            override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
-                super.onScrollStateChanged(recyclerView, newState)
-
-                scrollSum = 0
-
-                // Hide scroll down button, if reached bottom of the list
-                if (!recyclerView.canScrollVertically(1)) {
-                    viewState.hideScrollDownButton()
-                }
-
-                if (newState == RecyclerView.SCROLL_STATE_IDLE) {
-                    lastScrollPosition = (recyclerView.layoutManager as LinearLayoutManager).findFirstVisibleItemPosition()
-                }
-            }
-        }
-    }
 
     private fun initLayoutChangeListener() {
         layoutChangeListener =
